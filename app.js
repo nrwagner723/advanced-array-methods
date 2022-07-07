c255e6d8d3a816d56ff746e002a7835dd7f5d995
Natalie Wagner <nrwagner723@gmail.com>
2022-07-07 14:40:57 -0500

    question 2

--- app.js
+++ app.js
@@ -143,6 +143,20 @@ console.log("Vegetarian Foods: ", vegetarianFood)
 //2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
 
+function findItalianFood () {
+  let results = dishes.filter(function (el) {
+    if (el.cuisine === "Italian" && el.servings > 5) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return results
+}
+
+let ItalianFood = findItalianFood();
+console.log("Italian Foods with Serving Size Greater than 5: ", ItalianFood)
+
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
 
